93630772cedd437ac790eca6794475d33e76bfd4
Matt Romano <mromano592@gmail.com>
2026-07-11 17:30:29 -0400

    Add uthash methods and fix filesystem portability
    - Implement uthash add, find, delete, and deleteall methods.
    
    - Replace dirent->d_type check with stat() for better portability.

--- dirsyncd.c
+++ dirsyncd.c
@@ -1,6 +1,8 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/inotify.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include <string.h>
 #include <stdint.h>
@@ -8,19 +10,57 @@
 
 #include "uthash.h"
 
-#define WATCH_PATH "~/Projects"
+#define WATCH_PATH "/home/matt/Projects"
 #define MAX_PATH_LEN 1024	// 1KB
 
 static uint32_t event_mask = IN_CLOSE_WRITE | IN_MOVED_TO | IN_MOVED_FROM | IN_CREATE |  IN_DELETE | IN_DELETE_SELF;
 
-struct wd_path_pair {
+struct wddir {
 	int wd;			// key
 	const char* path;	
 	UT_hash_handle hh;	// Makes structure hashable via uthash
 };
 
-struct wd_path_pair* dir_hashmap = NULL;
+struct wddir* hashmap = NULL;
 
+// ========================================================
+// uthash.h methods for hashmap handling
+// ========================================================
+
+void add_wddir(int wd, const char* path) {
+    	struct wddir* entry;
+
+    	entry = malloc(sizeof *entry);
+    	entry->wd = wd;
+    	strcpy(entry->path, path);
+    	HASH_ADD_INT(hashmap, wd, entry);  /* wd: name of key field */
+}
+
+struct wddir* find_user(int wd_key) {
+        struct wddir* entry;
+
+	HASH_FIND_INT(hashmap, &wd_key, entry);  /* entry: desired output pointer */
+	return entry;
+}
+
+void delete_wddir(struct wddir* entry, int ininst_fd) {
+    	HASH_DEL(hashmap, entry);	/* entry: pointer to delete */
+	inotify_rm_watch(ininst_fd, entry->wd);	/* removes watch from inotify instance */
+	free(entry);            
+}
+
+void delete_all_wddir(int ininst_fd) {
+ 	struct wddir *current_entry, *tmp;
+  	HASH_ITER(hh, hashmap, current_entry, tmp) {
+    		delete_wddir(current_entry, ininst_fd);		/* delete; entry advances to next */
+		//HASH_DEL(hashmap, current_entry);
+		//free(current_entry);        
+  	}
+}
+
+// ========================================================
+// Reads linux system files to find watch descriptor max
+// ========================================================
 
 int sys_wdmax() {
 	FILE* fp = fopen("/proc/sys/fs/inotify/max_user_watches", "r");
@@ -45,7 +85,7 @@ int sys_wdmax() {
 	return (uint32_t)max_wd;
 }
 
-int dir_scan_recursive(const char* base_path, ininst_fd) {
+int dir_scan_recursive(const char* base_path, int ininst_fd) {
 	DIR* dir = opendir(base_path);
 	
 	// Unable to open directory
@@ -58,29 +98,33 @@ int dir_scan_recursive(const char* base_path, ininst_fd) {
 		return -1;
 	}
 
+	// Add to hashmap
+        add_wddir(wd, base_path);
+
 	struct dirent* entry;
 	while ((entry = readdir(dir)) != NULL) {
 		// Ignore . and ..
 		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
 			continue;
 		
-		// Directory found --> Add to kernel's inotify watch list
-		if (entry->d_type == DT_DIR) {
-			char dir_path[MAX_PATH_LEN];
-			snprintf(dir_path, MAX_PATH_LEN, "%s/%s", base_path, entry->d_name);
-			
-			// Add to hash
-
-			dir_scan_recursive(dir_path, ininst_fd);	
+		// Construct entry path
+		char entry_path[MAX_PATH_LEN];
+                snprintf(entry_path, MAX_PATH_LEN, "%s/%s", base_path, entry->d_name);
+		
+		// Entry is a dir --> Add to kernel's inotify watch list
+		struct stat sb;
+		if (stat(entry_path, &sb) && S_ISDIR(sb.st_mode)) {
+			dir_scan_recursive(entry_path, ininst_fd);	
 		}
 	}
 
 	// Directory layer is fully traversed
 	// Close associated DIR*
-	closedir(base_path);
+	closedir(dir);
 	
 	return 0;
 }
+
 
 int full_read(int fd, char* buf, int n) {
 	size_t bytes_read = 0;
@@ -88,13 +132,13 @@ int full_read(int fd, char* buf, int n) {
 	while (bytes_read < n) {
 		size_t result = read(fd, buf + bytes_read, n - bytes_read);
 
-		if (result < 0) {
+		if (result < 0) {	// Error
 			if (errno == EINTR)
 				continue;
 			return -1;
 		}
 
-		else if (result == EOF)
+		else if (result == 0)	// EOF
 			return -1;
 
 		bytes_read += result;
@@ -102,6 +146,7 @@ int full_read(int fd, char* buf, int n) {
 	
 	return 0;
 }
+
 
 int main() {
 	int ininst_fd = inotify_init();
@@ -111,7 +156,7 @@ int main() {
 	}
 		
 	int max_wd = sys_wdmax();
-	if (max_wd , 0) {
+	if (max_wd < 0) {
 		// Syslog
 		return -1;
 	}	
@@ -142,10 +187,11 @@ int main() {
 
 		// HANDLE EVENT
 		struct inotify_event* event = (struct inotify_event*)buf;
+		
 	}
 	
-	cleanup_wds(ininst_fd);
-	close(ininst_fd);
+	delete_all_wddir(ininst_fd);	// Frees all hashmap entries & removes all watch descriptors from kernel's inotify instance
+	close(ininst_fd);		// Closes inotify instance
 	return 0;	
 }
 
